82fddc95d1d51587cb946f81bea8925b95fc62e1
LAYONKGG <mlaouali374@gmail.com>
2025-05-22 10:21:45 +0100

    Modification du script count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-#Crée un script pour compter le nombre de fichiers dans un dossier
-#Vérifie si un argument est fourni
-if [ -z "$1" ]; then
-    echo "Usage : $O <nom_du_dossier>"
+
+#Vérifie que le répertoire est passé en argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage : $0 <répertoire>"
     exit 1
 fi
+
+#Assigner l'argument à une variable
+repertoire="$1"
+
 #Vérifie si le répertoire existe
-if [ ! -d "$1" ]; then
-    echo "Erreur : '$1'  n'est pas un dossier valide."
+if [ ! -d "$repertoire" ]; then
+    echo "Erreur : Le répertoire '$repertoire' n'existe pas."
     exit 1
 fi
+
+#Compte le nombre de fichiers dans le répertoire
+count=$(ls -l "$repertoire" | grep -v ^d | wc -l) 
+
+echo "Le répertoire '$repertoire' contient $count fichier(s)."
