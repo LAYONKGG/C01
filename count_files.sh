4116ee8aa98d5903cd4a01e21a7a79664c3c2c66
LAYONKGG <mlaouali374@gmail.com>
2025-05-28 14:51:48 +0100

    Ajout du script count_files.sh au dépôt

---
+++ count_files.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#lire le nom du dossier
+read myfolder
+
+#recuperer le nombre des fichier dans $myfolder
+nbre=$(ls "$myfolder" | wc -l)
+
+#si le nombre n'est pas null, afficher
+if [ "$nbre" -gt 0 ]; then
+
+    echo "Le dossier $myfolder contient $nbre fichier(s)."
+#sinon afficher zero
+else
+
+    echo "Le dossier $myfolder contient 0 fichier(s)."
+fi
+exit 0
